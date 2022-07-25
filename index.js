99b0f78ae1388f0b1b2e25f5fbe6f5b867b10331
Kartik-Vyas <101496656+Kartik-Vyas@users.noreply.github.com>
2022-07-25 14:27:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,3 +58,13 @@ function makeSound(key){
             break;
     }
 }
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
